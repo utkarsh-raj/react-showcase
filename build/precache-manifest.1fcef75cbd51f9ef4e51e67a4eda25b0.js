70f7c23ccae56d10002dca15c6d8e90519a57268
Utkarsh Raj Singh <u.rajsingh2503@gmail.com>
2020-08-15 16:41:54 +0530

    fixx

---
+++ build/precache-manifest.1fcef75cbd51f9ef4e51e67a4eda25b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d853f1a83ad8ddeaef1a1a704e86183c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e131413152847493838",
+    "url": "/static/js/2.eded5c32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eded5c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9adcdf3bed5e5cb2ca1b",
+    "url": "/static/js/main.ee8582b9.chunk.js"
+  },
+  {
+    "revision": "2995a34acc1574d4fde8",
+    "url": "/static/js/runtime-main.96f6465c.js"
+  }
+]);
